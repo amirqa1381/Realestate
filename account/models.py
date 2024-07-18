52a2e018e088b6e580c09233ac0f2061bd202342
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-18 17:14:35 +0330

    added some fields to the User model for customization and so on and so forth

--- account/models.py
+++ account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    this class is for making and customization od the user in the project
+    """
+    address = models.CharField(max_length=350, null=True, blank=True, verbose_name="Address")
+    city = models.CharField(max_length=150, verbose_name="City")
+    country = models.CharField(max_length=150, verbose_name="Country")
+    image = models.ImageField(upload_to='user-image', null=True, blank=True, verbose_name="Image")
+    phone = models.CharField(max_length=11, null=True, blank=True, verbose_name="Phone")
+
+    def __str__(self):
+        return self.username
+    
