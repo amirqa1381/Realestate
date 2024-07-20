ed3b681c489aba262256e76f5a4efb7d1d255141
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-20 09:17:34 +0330

    added  the main package to the installed app

--- realstate/settings.py
+++ realstate/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'account'
+    'account',
+    'main',
 ]
 
 MIDDLEWARE = [
