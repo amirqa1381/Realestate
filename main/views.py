4e3b29e5f33adf4acbf1f7998205d39a6adee036
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-22 15:04:09 +0330

    here i've created a class in the view instead of the function base view i want to use the class base view for it , and after that i added a rout to the urls file for handling the requests that are comming to that view

--- main/views.py
+++ main/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.views import View
 
-# Create your views here.
+
+class IndexView(View):
+    """
+    this class view is for showing the page that is the main page,
+    and we render the properties in this page
+    """
+
+    def get(self, request: HttpRequest):
+        """
+        this method is for the get method and it handel the get method request
+        """
+        return render(request, 'main/index.html')
+
+    def post(self, request: HttpRequest):
+        """
+        this method is for the post method and it handel the post method request
+        """
+        pass
