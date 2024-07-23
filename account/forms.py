e1a6f4fe8bd9b2183a8578c39b242bfa88f09dbd
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-23 11:17:00 +0330

    here we have some modification first of all i've added the password1 and password2 for giving them widget, and after that i've changed the first_name and last_name as optional and change the required of them to false

--- account/forms.py
+++ account/forms.py
@@ -8,6 +8,10 @@ class RegistrationForm(UserCreationForm):
     this class is for user registration and here we inherit from the UserCreationForm that
     was defined in the django by default for normal usage
     """
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label='Password',
+                                required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}),
+                                label='Confirmation Password', required=True)
 
     class Meta:
         model = User
@@ -15,6 +19,11 @@ class RegistrationForm(UserCreationForm):
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
-            'first_name': forms.TextInput(attrs={'class': 'from-control'}),
-            'last_name': forms.TextInput(attrs={'class': 'from-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Optional'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].required = False
+        self.fields['last_name'].required = False
