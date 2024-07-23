05d372ee14a170febbe260c0daaa2e5494ed650e
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-23 10:03:52 +0330

    in the admin file i've added all the models that i've define in the models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Home, Rent, Repair, Sell
 
-# Register your models here.
+
+@admin.register(Home)
+class HomeAdmin(admin.ModelAdmin):
+    """
+    this class is for the home model, and it created for showing the fields of the home in the admin page
+    """
+    list_display = ['owner', 'make_address_shorter', 'meter', 'is_active']
+    list_filter = ['meter', 'city', 'country', 'is_active']
+
+
+admin.site.register(Rent)
+admin.site.register(Repair)
+admin.site.register(Sell)
