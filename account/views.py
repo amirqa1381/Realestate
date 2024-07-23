dbb49bde24a199f22d01cd915c3e96a7e5c9cebb
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-23 11:19:05 +0330

    in this block of code i've wrote a class for handling the registration

--- account/views.py
+++ account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from .forms import RegistrationForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class UserRegistrationView(FormView):
+    form_class = RegistrationForm
+    success_url = reverse_lazy('index')
+    template_name = 'account/register.html'
+    
+    def form_valid(self, form):
+        user = form.save()
+        return super().form_valid(form)
