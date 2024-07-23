4a407d42cc0fd3fd5991847aff9b9cca7881363a
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-23 09:22:23 +0330

    in the admin of the account app i've added two class that register the account and main in the admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import User, RealEstate
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name')
+    fieldsets = [
+        (
+            None,
+            {'fields': ['username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'address']},
+        ),
+        (
+            'Advanced Option',
+            {
+                "classes": ["collapse"],
+                "fields": ["city", "country", "phone", "image"],
+            }
+        )
+    ]
+
+
+@admin.register(RealEstate)
+class RealEstateAdmin(admin.ModelAdmin):
+    list_display = ['ceo', 'is_guarantee', 'city', 'country']
