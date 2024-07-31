8096b8611a3cf48002927b5d67f36ad90445cc1e
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-31 11:13:46 +0330

    Added the blogs route to the url for handling the requests that we sent to that view

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import IndexView, AboutView
+from .views import IndexView, AboutView, BlogListView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('about/', AboutView.as_view(), name='about'),
+    path('blogs/', BlogListView.as_view(), name='blogs'),
 ]
