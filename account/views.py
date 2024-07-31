f4a9ebaf5e7b8d40ed4193af63d73953f2591408
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-07-31 08:57:36 +0330

    created a class for the contact and modified some thing in the contact.html

--- account/views.py
+++ account/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import logging
 from django.views.generic import FormView
 from django.contrib.auth.views import LoginView
-from .forms import RegistrationForm, LoginForm
+from .forms import RegistrationForm, LoginForm, ContactForm
 from django.urls import reverse_lazy
-
+from django.views import View
+from django.http import HttpRequest
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 logger = logging.getLogger(__name__)
 
@@ -37,3 +39,36 @@ class UserLoginView(LoginView):
     def form_invalid(self, form):
         logger.warning(f"Failed login attempt for user {form.cleaned_data['username']}")
         return super().form_invalid(form)
+
+
+class ContactView(LoginRequiredMixin, View):
+    """
+    this class is for the contact form, and we should use the FormView, but I decided to use
+    the View for writing this view and I think it's good practice for doing it.
+    """
+
+    def get(self, request: HttpRequest):
+        """
+        this is function for handling the get request that comes to the view and rout that we defined
+        """
+        form = ContactForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'account/contact.html', context)
+
+    def post(self, request: HttpRequest):
+        """
+        this is the function that we wrote, and it's responsible for the post method and it handel the
+        requests that come to this view.
+        """
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.author = request.user
+            form.save()
+            return redirect('index')
+        context = {
+            'form': form
+        }
+        return render(request, 'account/contact.html', context)
