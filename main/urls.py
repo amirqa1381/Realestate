7aad1884eedf2178b5a67c45ebc95072388dd46e
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-08-02 12:45:44 +0330

    Added the route for the property.html and Property list view

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView, AboutView, BlogListView
+from .views import IndexView, AboutView, BlogListView, PropertyListView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('about/', AboutView.as_view(), name='about'),
     path('blogs/', BlogListView.as_view(), name='blogs'),
+    path('properties/', PropertyListView.as_view(), name='properties'),
 ]
