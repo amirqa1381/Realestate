b199d4c5c5ed206bb277a8b8058fa7ab84622dab
Amir Ghasemian <amirghasemian1381@gmail.com>
2024-09-24 18:22:01 +0330

    i have created a function for handling the geting the post items

--- search/views.py
+++ search/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.urls import reverse
 
-# Create your views here.
+
+def serach_func(request:HttpRequest):
+    qu = request.POST.get("query", None)
+    bed = request.POST.get("bed", None)
+    print(qu)
+    print(bed)
+    return redirect(reverse("index"))   
