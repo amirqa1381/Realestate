b41f001f392c5fcef80b2cc19f483c6db01f5e37
Amir Hussein <amirghasemian1381@gmail.com>
2024-10-30 14:50:03 +0330

    in the form i have created two class for the forms and they are for the repair and images for the repairing

--- repair/forms.py
+++ repair/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import Mechanic
+from .models import Mechanic, Repair, RepairImages
+from django.forms import modelformset_factory
 
 class MechanicalRegistrationForm(forms.ModelForm):
     """
@@ -11,3 +12,33 @@ class MechanicalRegistrationForm(forms.ModelForm):
         widgets = {
             'mechanical_engineering_code': forms.TextInput(attrs={'class':'form-control','placeholder':'The length should be 12 character'}),
         }    
+        
+
+
+class RepairForm(forms.ModelForm):
+    """
+    this is the class that i have and it's for the repairing 
+    """
+    class Meta:
+        model = Repair
+        fields = ['issue']
+        widgets = {
+            'issue': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+        
+
+
+class RepairImagesForm(forms.ModelForm):
+    """
+    this class is for adding the form images and handling the images for it
+    """
+    class Meta: 
+        model = RepairImages
+        fields = ['images', 'alt']
+        widgets = {
+            'images': forms.FileInput(attrs={'class': 'form-control'}),
+            'alt': forms.TextInput(attrs={'class': 'form-control'})
+        }
+        
+
+RepairImagesFormset = modelformset_factory(RepairImages, form=RepairImagesForm, extra=2, can_delete=True)
