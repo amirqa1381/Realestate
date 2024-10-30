71af9cb8acaeb9073e57b9469b3765d93a1ff702
Amir Hussein <amirghasemian1381@gmail.com>
2024-10-30 11:50:56 +0330

    added the repair house choosing to the url

--- repair/urls.py
+++ repair/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import MechanicRegister
+from .views import MechanicRegister, RepairChoosingHouse
 
 
 
 urlpatterns = [
     path("register-mechanic/", MechanicRegister.as_view(), name="mechanic_register"),
+    path("repair-choosing-house/", RepairChoosingHouse.as_view(), name="home_choosing_house"),
 ]
