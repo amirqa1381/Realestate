7719d971d78acb6b6d0906fdee6a7ed64b83c59d
Amir Hussein <amirghasemian1381@gmail.com>
2024-10-31 20:16:59 +0330

    i have added the repair submit view to the urls

--- repair/urls.py
+++ repair/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import MechanicRegister, RepairChoosingHouse
+from .views import MechanicRegister, RepairChoosingHouse, RepairSubmitView
 
 
 
 urlpatterns = [
     path("register-mechanic/", MechanicRegister.as_view(), name="mechanic_register"),
     path("repair-choosing-house/", RepairChoosingHouse.as_view(), name="home_choosing_house"),
+    path("repair-submit-request/<int:pk>/", RepairSubmitView.as_view(), name="repair_submit_view")
 ]
