e58ba9f6e317e1ef203ba89940add374f6c05832
Amir Hussein <amirghasemian1381@gmail.com>
2024-12-03 14:53:46 +0330

    added the view to the urls

--- loan_service/urls.py
+++ loan_service/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import BorrowerRegisterView, CheckingActivateCode, LoanServiceView
+from .views import (BorrowerRegisterView,
+                    CheckingActivateCode,
+                    LoanServiceView,
+                    WalletView,
+                    )
 
 
 urlpatterns = [
     path("borrower-register/", BorrowerRegisterView.as_view(), name='borrower_register'),
     path("checking-activate-code/", CheckingActivateCode.as_view(), name="checking_activate_code"),
-    path("loan-service/", LoanServiceView.as_view(), name="loan_service")
+    path("loan-service/", LoanServiceView.as_view(), name="loan_service"),
+    path("wallet/", WalletView.as_view(), name="wallet"),
 ]
