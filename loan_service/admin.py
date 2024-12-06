f108d1d13bae684a78c271828a643905770ee84d
Amir Hussein <amirghasemian1381@gmail.com>
2024-12-06 10:57:00 +0330

    i have registered the wallet successfully in the admin file

--- loan_service/admin.py
+++ loan_service/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import  Wallet
 
-# Register your models here.
+
+
+@admin.register(Wallet)
+class AdminWallet(admin.ModelAdmin):
+    """
+    this class is for the bring the wallet to the admin page , and we can create the wallet for each user
+    """
+    list_display = ['user', 'balance', 'created_at']
+
+
+
+
